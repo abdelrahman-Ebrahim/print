49009b0e8cf66a3d724de3e4f68db20888ca0745
abdelrahman-Ebrahim <abdelrahmanebrabima@gmail.com>
2025-07-07 13:40:51 +0300

    FAQs responsive updates

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,8 @@ export default function Home() {
       <Info />
       {/* <Features /> */}
       <Options />
-      {/* <FAQs />
-      <Contact />
+      <FAQs />
+      {/* <Contact />
       <Footer /> */}
     </>
   );
